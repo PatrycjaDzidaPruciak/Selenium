574a1994524c851e4597ce5bfd01d0ec54a624fd
fcb1987 <dzidapati@interia.pl>
2023-06-12 21:06:31 +0200

    Xpath Driver Home Page Login Page

--- src/main/java/org/example/testCase/Main.java
+++ src/main/java/org/example/testCase/Main.java
@@ -1,5 +1,7 @@
-package org.example;
+package org.example.testCase;
 
+import org.example.pages.HomePage;
+import org.example.pages.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -16,21 +18,15 @@ public class Main {
         WebDriver driver = new ChromeDriver(ops);
         driver.get("https://demoqa.com/books");
         driver.manage().window().maximize();
-        driver.findElement(By.id("login")).click();
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,350)", "");
-        driver.findElement(By.id("userName")).sendKeys("gunjankaushik");
-        driver.findElement(By.id("password")).sendKeys("Password@123");
-        driver.findElement(By.id("login")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
-        System.out.println("Jestes na stronie" + driver.getCurrentUrl());
-        driver.quit();
+        HomePage homepage = new HomePage(driver);
+        LoginPage loginpage = new LoginPage(driver);
+
+
+        homepage.loginClick();
+        loginpage.enterUserName("gunjankaushik");
+
+
     }
 }
 
